3e3fefad7c53e510443eb113809414b07a48dc76
JeniaDan <jdanilyuck@gmail.com>
2025-04-04 00:54:31 +0300

    fourth

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 1
+def input_text():
+    pass
+
+# 2
+def read_file():
+    pass
+
+# 3
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 1
+def output_text():
+    pass
+
+# 2
+def write_file(path: str):
+    pass
+
+# 3
+def write_file_pandas(path: str):
+    pass
